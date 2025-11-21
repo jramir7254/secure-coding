5e62da7c42bf6f9a60250958081de98110321017
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2025-11-21 10:01:31 -0700

    up

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,5 +4,6 @@ const URL = import.meta.env.VITE_API_URL
 
 // create and export ONE shared connection
 export const socket = io(URL, {
+    path: '/socket.io/',
     autoConnect: false, // optional: control when to connect
 });
