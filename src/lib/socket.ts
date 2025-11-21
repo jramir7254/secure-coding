ff015fe7d5c9a21f1da6f03b57ef610cab339520
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2025-11-21 13:20:04 -0700

    up8

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,7 +4,7 @@ const URL = import.meta.env.VITE_API_URL
 
 // create and export ONE shared connection
 export const socket = io('https://secure-coding.epcc.acm.org', {
-    path: "/ws/",
+    path: "/api/ws/",
     transports: ["websocket"],
 
     autoConnect: false, // optional: control when to connect
