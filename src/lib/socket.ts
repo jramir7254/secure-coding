7c4328f344e3f771911c5b6a8dbb148499990909
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2026-01-25 21:53:29 -0700

    Testing sovcket 2

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,6 +3,6 @@ import { io } from 'socket.io-client';
 const URL = import.meta.env.VITE_API_URL
 
 // create and export ONE shared connection
-export const socket = io('https://secure-coding.epcc.acm.org', {
+export const socket = io('https://secure-coding.epcc.acm.org/api', {
     autoConnect: false, // optional: control when to connect
 });
