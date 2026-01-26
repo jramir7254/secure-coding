ffbb524498e5a2e28d468b80c0d9e9133ff337a0
jramir7254 <83803191+jramir7254@users.noreply.github.com>
2026-01-25 22:00:40 -0700

    change path to /api

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -4,5 +4,6 @@ const URL = import.meta.env.VITE_API_URL
 
 // create and export ONE shared connection
 export const socket = io('https://secure-coding.epcc.acm.org/api', {
+    path: '/api',
     autoConnect: false, // optional: control when to connect
 });
